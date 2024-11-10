8646492883174ae41566b6547f895f2f2255447d
zhouh11 <zhouh11@rpi.edu>
2024-11-10 00:07:05 -0500

    edit

--- air_quality.js
+++ air_quality.js
@@ -1,42 +1,43 @@
 const apiKey = "17f9fbda-4033-435c-8dfb-54634b5d112f";
-// const state = document.getElementById("state").value;
-// const city = document.getElementById("city").value;
 
 const apiUrl = `http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=USA&key=${apiKey}`;
-// const apiUrl = "http://api.airvisual.com/v2/city?city=Los Angeles&state=California&country=USA&key=17f9fbda-4033-435c-8dfb-54634b5d112f";
-
 
 document.getElementById("search_btn").addEventListener("click", function() {
+
+   
     const state = document.getElementById("state").value;
     const city = document.getElementById("city").value;
 
 
     if (state && city) {
-        const apiUrl = `http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=USA&key=${apiKey}`;
+        location.href = `./info.html?city=${city}&state=${state}`;
         
-        // Fetch data from the API
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                // Handle the API response
-                const city_ = data.data.city;
-                const state_ = data.data.state;
-                const airQuality = data.data.current.pollution.aqius;
-                const mainPollutant  = data.data.current.pollution.mainus;
+        // const apiUrl = `http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=USA&key=${apiKey}`;
 
-                const outputCity = document.getElementById('_City');
-                outputCity.innerHTML = city_;
+        // fetch(apiUrl)
+        //     .then(response => response.json())
+        //     .then(data => {
+        //         // Handle the API response
+        //         const city_ = data.data.city;
+        //         const state_ = data.data.state;
+        //         const airQuality = data.data.current.pollution.aqius;
+        //         const mainPollutant  = data.data.current.pollution.mainus;
 
-                const outputState = document.getElementById('_State');
-                outputState.innerHTML = state_;
+        //         const outputCity = document.getElementById('_City');
+        //         outputCity.innerHTML = city_;
 
-                const outputAirQuality = document.getElementById('airQuality');
-                outputAirQuality.innerHTML = "Air Quality: " + airQuality;
+        //         const outputState = document.getElementById('_State');
+        //         outputState.innerHTML = state_;
 
-                const outputMainPollutant = document.getElementById('mainPollutant');
-                outputMainPollutant.innerHTML = "Main Pollutant: " + mainPollutant;
-            })
-            .catch(error => console.error('Error fetching data:', error));
+        //         const outputAirQuality = document.getElementById('airQuality');
+        //         outputAirQuality.innerHTML = "Air Quality: " + airQuality;
+
+        //         const outputMainPollutant = document.getElementById('mainPollutant');
+        //         outputMainPollutant.innerHTML = "Main Pollutant: " + mainPollutant;
+
+        //          location.href = `./info.html?city=${city_}&state=${state_}&airQuality=${airQuality}&mainPollutant=${mainPollutant}`;
+        //     })
+        //     .catch(error => console.error('Error fetching data:', error));
     } else {
         alert("Please enter both city and state.");
     }
--- state.js
+++ state.js
@@ -1,16 +1,11 @@
 const apiKey = "17f9fbda-4033-435c-8dfb-54634b5d112f";
 const urlParams = new URLSearchParams(window.location.search);
 const state = urlParams.get('state');
-document.getElementById('state').innerText = state;
+document.getElementById('State').innerText = state;
 
 
 
 document.getElementById("city_btn").addEventListener("click", function() {
-    // const urlParams = new URLSearchParams(window.location.search);
-    // const state = urlParams.get('state');
-
-    // outputInput(state);
-    // const apiKey = "17f9fbda-4033-435c-8dfb-54634b5d112f";
     const apiUrl = `http://api.airvisual.com/v2/cities?state=${state}&country=USA&key=${apiKey}`;
      // Fetch data from the API
      fetch(apiUrl)
@@ -27,51 +22,40 @@ document.getElementById("city_btn").addEventListener("click", function() {
     })
     .catch(error => console.error('Error fetching data:', error));
 
-
 });
 
 
+// document.getElementById("search_btn").addEventListener("click", function() {
+//     const city = document.getElementById("city").value;
 
-
-document.getElementById("search_btn").addEventListener("click", function() {
-    const city = document.getElementById("city").value;
-
-
-    if (state && city) {
-        const apiUrl = `http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=USA&key=${apiKey}`;
+//     if (state && city) {
+//         const apiUrl = `http://api.airvisual.com/v2/city?city=${city}&state=${state}&country=USA&key=${apiKey}`;
         
-        // Fetch data from the API
-        fetch(apiUrl)
-            .then(response => response.json())
-            .then(data => {
-                // Handle the API response
-                const city_ = data.data.city;
-                const state_ = data.data.state;
-                const airQuality = data.data.current.pollution.aqius;
-                const mainPollutant  = data.data.current.pollution.mainus;
+//         // Fetch data from the API
+//         fetch(apiUrl)
+//             .then(response => response.json())
+//             .then(data => {
+//                 // Handle the API response
+//                 const city_ = data.data.city;
+//                 const state_ = data.data.state;
+//                 const airQuality = data.data.current.pollution.aqius;
+//                 const mainPollutant  = data.data.current.pollution.mainus;
 
-                const outputCity = document.getElementById('_City');
-                outputCity.innerHTML = city_;
+//                 const outputCity = document.getElementById('_City');
+//                 outputCity.innerHTML = city_;
 
-                const outputState = document.getElementById('_State');
-                outputState.innerHTML = state_;
+//                 const outputState = document.getElementById('_State');
+//                 outputState.innerHTML = state_;
 
-                const outputAirQuality = document.getElementById('airQuality');
-                outputAirQuality.innerHTML = airQuality;
+//                 const outputAirQuality = document.getElementById('airQuality');
+//                 outputAirQuality.innerHTML = airQuality;
 
-                const outputMainPollutant = document.getElementById('mainPollutant');
-                outputMainPollutant.innerHTML = mainPollutant;
-            })
-            .catch(error => console.error('Error fetching data:', error));
-    } else {
-        alert("Please enter both city and state.");
-    }
-});
+//                 const outputMainPollutant = document.getElementById('mainPollutant');
+//                 outputMainPollutant.innerHTML = mainPollutant;
+//             })
+//             .catch(error => console.error('Error fetching data:', error));
+//     } else {
+//         alert("Please enter both city and state.");
+//     }
+// });
 
-// function generateAllCity(state){
-
-// }
-
-function outputInput(state) {
-    document.getElementById("output").textContent = `State: ${state}`;
-}
